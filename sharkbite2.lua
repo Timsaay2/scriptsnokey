3a894aabae92dc66913f45500c3ad715dceae891
Timsaay2 <117690060+Timsaay2@users.noreply.github.com>
2022-11-08 14:04:40 +1100

    Update sharkbite2.lua

--- sharkbite2.lua
+++ sharkbite2.lua
@@ -3,45 +3,69 @@
 
 -- Instances:
 
-local toggle = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local TextButton = Instance.new("TextButton")
-local TextLabel = Instance.new("TextLabel")
+local PapaHub = Instance.new("ScreenGui")
+local Main = Instance.new("Frame")
+local Title = Instance.new("TextLabel")
+local Credits = Instance.new("TextLabel")
+local Activate = Instance.new("TextButton")
 
 --Properties:
 
-toggle.Name = "toggle"
-toggle.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-toggle.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+PapaHub.Name = "PapaHub"
+PapaHub.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+PapaHub.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-Frame.Parent = toggle
-Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Frame.Position = UDim2.new(0.166493237, 0, 0.125636131, 0)
-Frame.Size = UDim2.new(0, 137, 0, 139)
+Main.Name = "Main"
+Main.Parent = PapaHub
+Main.Active = true
+Main.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
+Main.BorderSizePixel = 0
+Main.Draggable = true
+Main.Position = UDim2.new(0.321207851, 0, 0.409807354, 0)
+Main.Size = UDim2.new(0, 295, 0, 116)
 
-TextButton.Parent = Frame
-TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextButton.Position = UDim2.new(0.129744455, 0, 0.139624283, 0)
-TextButton.Size = UDim2.new(0, 100, 0, 100)
-TextButton.Font = Enum.Font.SourceSans
-TextButton.Text = "TOGGLE"
-TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextButton.TextSize = 14.000
+Title.Name = "Title"
+Title.Parent = Main
+Title.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+Title.BorderSizePixel = 0
+Title.Size = UDim2.new(0, 295, 0, 16)
+Title.Font = Enum.Font.GothamBold
+Title.Text = "PapaHub AutoFarm"
+Title.TextColor3 = Color3.fromRGB(255, 255, 255)
+Title.TextScaled = true
+Title.TextSize = 12.000
+Title.TextWrapped = true
 
-TextLabel.Parent = Frame
-TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.BackgroundTransparency = 1.000
-TextLabel.Position = UDim2.new(-0.00138998032, 0, 0.994174123, 0)
-TextLabel.Size = UDim2.new(0, 137, 0, 42)
-TextLabel.Font = Enum.Font.SourceSans
-TextLabel.Text = "discord.gg/882ukwB58k"
-TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.TextSize = 14.000
+Credits.Name = "Credits"
+Credits.Parent = Main
+Credits.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+Credits.BorderSizePixel = 0
+Credits.Position = UDim2.new(0, 0, 0.861901641, 0)
+Credits.Size = UDim2.new(0, 295, 0, 16)
+Credits.Font = Enum.Font.GothamBold
+Credits.Text = "discord.gg/882ukwB58k"
+Credits.TextColor3 = Color3.fromRGB(255, 255, 255)
+Credits.TextScaled = true
+Credits.TextSize = 12.000
+Credits.TextWrapped = true
+
+Activate.Name = "Activate"
+Activate.Parent = Main
+Activate.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+Activate.BorderColor3 = Color3.fromRGB(27, 42, 53)
+Activate.BorderSizePixel = 0
+Activate.Position = UDim2.new(0.0330629945, 0, 0.243326917, 0)
+Activate.Size = UDim2.new(0, 274, 0, 59)
+Activate.Font = Enum.Font.GothamBold
+Activate.Text = "Toggle"
+Activate.TextColor3 = Color3.fromRGB(0, 255, 127)
+Activate.TextSize = 43.000
+Activate.TextStrokeColor3 = Color3.fromRGB(102, 255, 115)
 
 -- Scripts:
 
-local function TAIHZMO_fake_script() -- TextButton.LocalScript 
-	local script = Instance.new('LocalScript', TextButton)
+local function ETKUX_fake_script() -- Activate.LocalScript 
+	local script = Instance.new('LocalScript', Activate)
 
 	toggle = false
 	script.Parent.MouseButton1Down:connect(function()
@@ -53,15 +77,21 @@ local function TAIHZMO_fake_script() -- TextButton.LocalScript
 		end
 		while toggle == true do
 			wait()
+			print("Toggle turned on")
 			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace")["island5(Small)"].Model.MeshPart.CFrame
 			game:GetService("Players").LocalPlayer.PlayerGui.AFK.Enabled = false
 			game:GetService("ReplicatedStorage").EventsFolder.GameLoop:ClearAllChildren()
 			game.Players.LocalPlayer.Character.Humanoid.Sit = false 	
+			local vu = game:GetService("VirtualUser")
+			game:GetService("Players").LocalPlayer.Idled:connect(function()
+				vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+				wait(1)
+				vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+			end)
 		end
 		if toggle == false then 
 			print("Toggle turned off")
 		end
 	end)
-	
 end
-coroutine.wrap(TAIHZMO_fake_script)()
+coroutine.wrap(ETKUX_fake_script)()

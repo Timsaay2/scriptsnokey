22dbc5f6d6cc0b1be908dc9fcf1bbbca3e5bd2bd
Timsaay2 <117690060+Timsaay2@users.noreply.github.com>
2022-11-26 11:40:52 +1100

    Update longestanswerwins.lua

--- longestanswerwins.lua
+++ longestanswerwins.lua
@@ -16,11 +16,20 @@ b:Toggle("AutoFarm",function(bool)
 	end
 end)
 
+b:Slider("Cooldown",{
+	min = 1; -- min value of the slider
+	max = 10; -- max value of the slider
+	precise = false; -- max 2 decimals
+},function(value)
+	cooldown = value 	
+	print(value)
+end)
+
 c:Toggle("AntiAFK",function(bool)
 	getgenv().antiAFK = bool
 	print("Status: " , bool)
 	if bool then 
-		doaAtiAFK()
+		antiAFK()
 	end
 end)
 
@@ -30,21 +39,23 @@ function autoFarm()
 			local args = {
 				[1] = answer.Value
 			}
-				game:GetService("ReplicatedStorage").SubmittedAnswer:FireServer(unpack(args))
-	wait(5)
+			game:GetService("ReplicatedStorage").SubmittedAnswer:FireServer(unpack(args))
+			wait(cooldown)
 		end 
 	end)
 end
-function doAntiAFK()
+function antiAFK()
 	spawn(function()
 		while getgenv().antiAFK == true do 
+			wait(60)
 			local vu = game:GetService("VirtualUser")
 			game:GetService("Players").LocalPlayer.Idled:connect(function()
 				vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 				vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-				wait()
 			end)
 		end
 	end)
 end
+
+
 

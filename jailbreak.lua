24f6cf736e9e41c86e4122df1f00b5d9c0b25c0c
Timsaay2 <117690060+Timsaay2@users.noreply.github.com>
2022-12-06 13:14:32 +1100

    Update jailbreak.lua

--- jailbreak.lua
+++ jailbreak.lua
@@ -1,6 +1,6 @@
    game.StarterGui:SetCore("SendNotification", {
-        Title = "(Script is now paid!)",
-        Text = "(#support to buy)"
+        Title = "SCRIPT IS NOW PAID!",
+        Text = "#support (to buy)"
     })
     
     setclipboard('https://discord.gg/tzDDY6mjWd')

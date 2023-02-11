29c322b06f3c4f8a18fc5b335eaa047eec57d5d3
Timsaay2 <117690060+Timsaay2@users.noreply.github.com>
2023-02-12 10:18:15 +1100

    Create chatbypasser.lua

---
+++ chatbypasser.lua
@@ -0,0 +1,8 @@
+local Main = game:GetService("CoreGui").Rayfield:WaitForChild("Main") 
+while Main.visible == true and Main.Topbar.Title.Text ~= 'PapaHub' 
+    
+wait()
+game:GetService("CoreGui").Rayfield.Main.Topbar.Title.Text = 'PapaHub'
+Main.TabList.Credits.Visible == False
+print('Loop')
+end 

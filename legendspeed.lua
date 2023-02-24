792485b98b40b022c509505490ba9240d0be925b
Timsaay2 <117690060+Timsaay2@users.noreply.github.com>
2023-02-25 10:19:35 +1100

    Update legendspeed.lua

--- legendspeed.lua
+++ legendspeed.lua
@@ -1,1 +1,4 @@
-script = "https://raw.githubusercontent.com/ReeAndArceus/Legends-Of-Speed-Gui/main/Gui"
+function correckey()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/ReeAndArceus/Legends-Of-Speed-Gui/main/Gui'),true))()
+end 
+

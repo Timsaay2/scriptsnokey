5ae18f021261e6b421d392a4d21a7292f1cf8238
Timsaay2 <117690060+Timsaay2@users.noreply.github.com>
2023-03-02 17:01:59 +1100

    Create 2playermillionairetycoon.lua

---
+++ 2playermillionairetycoon.lua
@@ -0,0 +1,13 @@
+
+_G.RedGUI = true
+_G.Theme = "Dark" -- Must disable or remove _G.RedGUI to use
+--Themes: Light, Dark, Mocha, Aqua and Jester
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/2PlayerMillionaireTycoon.lua"))()
+
+while true do 
+    wait (5)
+loadstring(game:HttpGet("https://pastebin.com/raw/9hGa82eS", true))()
+game:GetService("Workspace").Timsaay.HumanoidRootPart.CFrame = game:GetService("Workspace").Tycoons.Red1.Important.Door.CFrame
+
+end 

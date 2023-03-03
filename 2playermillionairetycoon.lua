dbd36fb0e9da8e3c0d96019ef2c4c461d9970c03
Timsaay2 <117690060+Timsaay2@users.noreply.github.com>
2023-03-03 16:33:04 +1100

    Update 2playermillionairetycoon.lua

--- 2playermillionairetycoon.lua
+++ 2playermillionairetycoon.lua
@@ -1,9 +1,17 @@
-
 _G.RedGUI = true
 _G.Theme = "Dark" -- Must disable or remove _G.RedGUI to use
 --Themes: Light, Dark, Mocha, Aqua and Jester
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/2PlayerMillionaireTycoon.lua"))()
+local title = game:GetService("CoreGui")["2 Player Millionaire Tycoon | discord.gg/rips | By RIP#6666"].MainFrame.TitleBar.Title
+while title.Text ~= 'PapaHub' do 
+    wait()
+print("Not loaded")
+game:GetService("CoreGui")["2 Player Millionaire Tycoon | discord.gg/rips | By RIP#6666"].MainFrame.NavBar.Content.CREDITS:destroy();
+game:GetService("CoreGui")["2 Player Millionaire Tycoon | discord.gg/rips | By RIP#6666"].MainFrame.NavBar.Content.FPS:destroy();
+ title.Text  = 'PapaHub'
+end 
+
 
 while true do 
     wait (5)

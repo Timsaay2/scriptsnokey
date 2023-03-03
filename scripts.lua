4647dce81e162948608505d01ae928feae76863a
Timsaay2 <117690060+Timsaay2@users.noreply.github.com>
2023-03-03 16:38:23 +1100

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -4,7 +4,7 @@ games = {
 	[3101667897] = 'https://raw.githubusercontent.com/ReeAndArceus/Legends-Of-Speed-Gui/main/Gui'; --legends of speed
 	[60684962] = 'https://scripts.luawl.com/14245/JailbreakerFree.lua'; -- jailbreak 
 	[537413528] = 'https://raw.githubusercontent.com/1201for/littlegui/main/Build-A-Boat'; -- buildaboat
-	
+	[6772424226] = 'https://raw.githubusercontent.com/Timsaay2/scriptsnokey/main/2playermillionairetycoon.lua'; -- 2 player millionaire tycoon
 	
 }
 

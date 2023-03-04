efef66fd2a0b3d37b0dc0c506f352a9f012d01b0
Timsaay2 <117690060+Timsaay2@users.noreply.github.com>
2023-03-04 12:54:35 +1100

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -5,7 +5,7 @@ games = {
 	[60684962] = 'https://scripts.luawl.com/14245/JailbreakerFree.lua'; -- jailbreak 
 	[537413528] = 'https://raw.githubusercontent.com/1201for/littlegui/main/Build-A-Boat'; -- buildaboat
 	[6772424226] = 'https://raw.githubusercontent.com/Timsaay2/scriptsnokey/main/2playermillionairetycoon.lua'; -- 2 player millionaire tycoon
-	
+	[815405518] = 'https://raw.githubusercontent.com/Timsaay2/scriptsnokey/main/thefloorislava.lua'; -- the floor is lava
 }
 
 

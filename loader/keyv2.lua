51e90e410e26db818248fbdbd645b0a4862ed326
Timsaay2 <117690060+Timsaay2@users.noreply.github.com>
2023-07-01 23:01:34 +1000

    Create keyv2.lua

---
+++ loader/keyv2.lua
@@ -0,0 +1,10 @@
+local keys = {
+    ['Monday']    = 'RstUvwxYZ#$',
+    ['Tuesday']   = 'Qa%%2xPd9W7hK',
+    ['Wednesday'] = 'jU6c$Ym3F1qV',
+    ['Thursday']  = 'sX9l2Hf#T8kR',
+    ['Friday']    = 'bN7v1Zq$M3pG',
+    ['Saturday']  = 'dC6j$K9n5L1v',
+    ['Sunday']    = 'aT5j2Bd9C4q$'
+}
+
